39bd54f6e38e98a564e72979472e0e5aa4a8e72c
Md Abdur Rahim <easinarafat.rahim@gmail.com>
2024-12-01 12:29:34 +0600

    modifying

--- A_King_Keykhosrow_s_Mystery.cpp
+++ A_King_Keykhosrow_s_Mystery.cpp
@@ -8,7 +8,7 @@ int main(){
     while(t--){
         int a,b,c;
         cin >> a >> b;
-        for(ll i=min(a,b);i<(min(a,b)+1000000000);i++){
+        for(ll i=min(a,b);i<(min(a,b)+1000000);i++){
             if(i%a==i%b){
                 c=i;break;
             }
